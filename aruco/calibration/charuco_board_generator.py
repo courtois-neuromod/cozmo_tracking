284d6b75d21803b0abb35da503f7bf873997decb
ADebor <antoine.debor@student.uliege.be>
2022-04-27 17:18:18 -0400

    cleaned up and from script to function

--- aruco/calibration/charuco_board_generator.py
+++ aruco/calibration/charuco_board_generator.py
@@ -3,16 +3,17 @@ import cv2.aruco as aruco
 import cv2
 import numpy as np
 
-squaresX = 5
-squaresY = 7
-squareLength = 0.04 # meters
-markerLength = 0.03 # meters
-dictionary = aruco.Dictionary_get(aruco.DICT_5X5_50)
 
-board = aruco.CharucoBoard_create(squaresX, squaresY, squareLength, markerLength, dictionary)
-img = 0
-img = board.draw((500, 700), marginSize=20)#, img, 1, 1)
+def make_board(squaresX, squaresY, squareLength, markerLength, dictionary):
 
-cv2.imwrite('charuco_board.jpg', img)
-cv2.imshow("board", img)
-cv2.waitKey(0)
+    board = aruco.CharucoBoard_create(
+        squaresX, squaresY, squareLength, markerLength, dictionary
+    )
+    img = 0
+    img = board.draw((500, 700), marginSize=20)  
+
+    cv2.imwrite("charuco_board.jpg", img)
+    # cv2.imshow("board", img)
+    # cv2.waitKey(0)
+
+    return board
